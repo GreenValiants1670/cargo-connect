37cee667ef7841621f20bd8c808a4a52a38a8b08
FortressBen <92190499+FortressBen@users.noreply.github.com>
2022-05-09 20:02:59 -0400

    changes 4 darkness ; pixel size is good

--- Camera Code.py
+++ Camera Code.py
@@ -1,4 +1,13 @@
-import sensor, image, time, pyb
+# Find Rects Example
+#
+# This example shows off how to find rectangles in the image using the quad threshold
+# detection code from our April Tags code. The quad threshold detection algorithm
+# detects rectangles in an extremely robust way and is much better than Hough
+# Transform based methods. For example, it can still detect rectangles even when lens
+# distortion causes those rectangles to look bent. Rounded rectangles are no problem!
+# (But, given this the code will also detect small radius circles too)...
+import sensor, image, time, pyb, utime
+
 SQUARES = 4
 sensor.reset()
 sensor.set_pixformat(sensor.GRAYSCALE) # grayscale is faster (160x120 max on OpenMV-M7)
@@ -7,13 +16,19 @@ sensor.set_auto_whitebal(False)
 sensor.set_auto_exposure(False,300)
 sensor.skip_frames(time = 2000)
 clock = time.clock()
-Threshold = (69, 255)
-indicator_green = pyb.Pin('P7', pyb.Pin.OUT_PP)
-indicator_red = pyb.Pin('P9', pyb.Pin.OUT_PP)
+Threshold = (45, 255)
+indicator_servo = pyb.Servo(2)
+'''indicator_red = pyb.Pin('P9', pyb.Pin.OUT_PP)
+while(True):
+    utime.sleep_ms(2000)
+    indicator_servo.angle(90)
+    indicator_red.value(0)
+    utime.sleep_ms(2000)
+    indicator_servo.angle(0)
+    indicator_red.value(1)'''  #debug stuff
 while(True):
     clock.tick()
     img = sensor.snapshot()
-
     # `threshold` below should be set to a high enough value to filter out noise
     # rectangles detected in the image which have low edge magnitudes. Rectangles
     # have larger edge magnitudes the larger and more contrasty they are...
@@ -22,22 +37,16 @@ while(True):
     list_o_blobs = 0
     for blob in blobs:
         print(blob.pixels())
-        if blob.pixels() <= 200 and blob.pixels() >= 80:
-            aspect_ratio = blob.h() / blob.w()
-            if aspect_ratio > 0.75 and aspect_ratio < 1.25:
-                img.draw_rectangle(blob.rect(), color=(255,255,255), thickness=2)
-                list_o_blobs += 1
-            else:
-                print(aspect_ratio)
-            
+        if blob.pixels() <= 150 and blob.pixels() >= 85:
+
+            list_o_blobs += 1
+
     if list_o_blobs == SQUARES:
          print("empty")
-         indicator_green.value(1)
-         indicator_red.value(0)
+         indicator_servo.angle(180)
     else:
         print("not empty")
-        indicator_green.value(0)
-        indicator_red.value(1)
+        indicator_servo.angle(0)
     print(list_o_blobs)
 
     print("FPS %f" % clock.fps())
